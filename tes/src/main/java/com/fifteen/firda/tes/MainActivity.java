37709d1afe5a5a1c75ee0ab3727a04325592a2e7
Your Name <you@example.com>
2019-06-28 23:20:10 +0300

    release v5

--- tes/src/main/java/com/fifteen/firda/tes/MainActivity.java
+++ tes/src/main/java/com/fifteen/firda/tes/MainActivity.java
@@ -23,23 +23,24 @@ import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    //private Button[] buttons = new Button[15];
-    private ArrayList<Button> nbuttons;
+    private ArrayList<Button> buttons;
     private TextView TimerTextView;
     private int buttonWidth, buttonHeight;
-    private boolean start = false;
     private int count = 0;
     private ArrayList<Button> listButtons = new ArrayList<>();
     private Timer mTimer;
     private MyTimerTask mMyTimerTask;
 
     private static final String TAG = "MainActivity";
+    //private static final String KEY_INDEX = "start";
+    //private boolean isStartGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //if (savedInstanceState != null)
+          //  isStartGame = savedInstanceState.getBoolean(KEY_INDEX, false);
         TimerTextView = findViewById(R.id.textView);
         listButtons.add((Button)findViewById(R.id.button2));
         listButtons.add((Button)findViewById(R.id.button3));
@@ -56,53 +57,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         listButtons.add((Button)findViewById(R.id.button14));
         listButtons.add((Button)findViewById(R.id.button16));
         listButtons.add((Button)findViewById(R.id.button18));
-
-
-        Button bb = findViewById(R.id.button3);
-        Log.d(TAG,  Float.toString(bb.getX()));
-        Log.d(TAG,  Float.toString(bb.getY()));
-        Log.d(TAG,  "!!!!!!!!!!!!!!!!!!!!");
-        //Collections.copy(nbuttons, listButtons);
-        nbuttons = new ArrayList<>(listButtons);
-        /*for (int i = 0; i < 15; i++) {
-            nbuttons[i] = listButtons.get(i);
-            Log.d(TAG,  Float.toString(nbuttons[i].getX()));
-            Log.d(TAG,  Float.toString(nbuttons[i].getY()));
-            Log.d(TAG,  "-----------------------------");
-        }*/
-
-        //l = Arrays.asList(buttons);
+        for (int i = 0; i < 15; i++)
+            listButtons.get(i).setOnClickListener(this);
+        buttons = new ArrayList<>(listButtons);
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
-
         buttonWidth = size.x / 4;
         buttonHeight = size.y / 4 - size.y / 28;
-        //contribution(false);
-        mTimer = new Timer();
-        mMyTimerTask = new MyTimerTask();
-        mTimer.schedule(mMyTimerTask, 0, 1000);
+        //if (!isStartGame)
         startGame(false);
     }
 
     public void startGame(boolean hard) {
+        //isStartGame = true;
         count = 0;
+        mTimer = new Timer();
+        mMyTimerTask = new MyTimerTask();
+        mTimer.schedule(mMyTimerTask, 0, 1000);
         for (int i = 0; i < 15; i++) {
             listButtons.get(i).setEnabled(true);
         }
-        //mTimer.purge();
-        //mMyTimerTask = new MyTimerTask();
-        start = true;
-        //mTimer.schedule(mMyTimerTask, 0, 1000);
         contribution(hard);
     }
 
     public  void finishGame() {
-        start = false;
-        //mTimer.cancel();
+        mTimer.cancel();
+        mTimer.purge();
+        mTimer = null;
+        mMyTimerTask = null;
         for (int i = 0; i < 15; i++) {
             listButtons.get(i).setEnabled(false);
         }
+        //isStartGame = false;
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -122,11 +109,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private boolean contribution(boolean hard) {
-        //count = 0;
-        //Log.d(TAG, "UUU");
-
         Collections.shuffle(listButtons);
-
         int result = 0;
         for (int i = 0; i < 15; i++) {
             for (int j = i; j < 15; j++) {
@@ -136,25 +119,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
         if (!hard && result % 2 == 1) {
-            //Log.d(TAG, "!hard && result % 2 == 1");
             return contribution(hard);
-            //Log.d(TAG, "AFTER !hard && result % 2 == 1");
         }
         else if (hard && result % 2 == 0) {
-            //Log.d(TAG, "hard && result % 2 == 0");
             return contribution(hard);
-            //Log.d(TAG, "AFTER hard && result % 2 == 0");
         }
-        Log.d(TAG, "AFTERr");
-
         for (int i = 0; i < 15; i++) {
-            //Log.d(TAG, "----------------------------------------------------");
-            //Log.d(TAG, (String) listButtons.get(i).getText());
-            //System.out.println("----------------------------------------------------");
-            //System.out.println(Integer.parseInt((String) l.get(i).getText())*2);
             listButtons.get(i).setX(0);
             listButtons.get(i).setY(0);
-            listButtons.get(i).setOnClickListener(this);
+            //listButtons.get(i).setOnClickListener(this);
             listButtons.get(i).setLayoutParams(new ConstraintLayout.LayoutParams(buttonWidth, buttonHeight));
             if (i < 4) {
                 listButtons.get(i).setX(i*buttonWidth);
@@ -172,11 +145,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 listButtons.get(i).setX((i - 12)*buttonWidth);
             }
         }
-        for (int i = 0; i < 15; i++) {
-            //Log.d(TAG,  Float.toString(nbuttons[i].getX()));
-            //Log.d(TAG,  Float.toString(nbuttons[i].getY()));
-            Log.d(TAG,  "-----------------------------");
-        }
         TimerTextView.setLayoutParams((new ConstraintLayout.LayoutParams(buttonWidth, buttonHeight)));
         TimerTextView.setX(3*buttonWidth);
         TimerTextView.setY(3*buttonHeight);
@@ -191,14 +159,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 @Override
                 public void run() {
-                    if (start) {
-                        TimerTextView.setText(Integer.toString(count));
-                        count += 1;
-                    }
+                    TimerTextView.setText(Integer.toString(count));
+                    count += 1;
                 }
             });
         }
     }
+
+    /*@Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_INDEX, isStartGame);
+    }*/
 
     private void check() {
         float x = 0;
@@ -208,14 +180,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 x = 0;
                 y += buttonHeight;
             }
-            if (nbuttons.get(i).getX() != x || nbuttons.get(i).getY() != y){
+            if (buttons.get(i).getX() != x || buttons.get(i).getY() != y){
                 break;
             }
             x += buttonWidth;
             if (i == 14) {
                 Toast.makeText(this, "You have done it in " + TimerTextView.getText() + " seconds", Toast.LENGTH_LONG).show();
-                //nbuttons[i].sete
-                //mTimer.cancel();
                 finishGame();
             }
         }
@@ -223,10 +193,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        /*if (!start) {
-            start = true;
-            mTimer.schedule(mMyTimerTask, 0, 1000);
-        }*/
         if (TimerTextView.getX() == v.getX() + buttonWidth && TimerTextView.getY() == v.getY()) {
             TimerTextView.setX(TimerTextView.getX() - buttonWidth); TimerTextView.setY(TimerTextView.getY());
             v.setX(v.getX() + buttonWidth); v.setY(TimerTextView.getY());
